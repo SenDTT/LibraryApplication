959aa78c02b7f9570f25bdd106bf476feae0ece1
SenDoan <mariathanhsen@gmail.com>
2024-10-11 16:28:53 -0500

    submit add book

--- src/dataaccess/TestData.java
+++ src/dataaccess/TestData.java
@@ -8,6 +8,7 @@ import business.Address;
 import business.Author;
 import business.Book;
 import business.LibraryMember;
+import librarysystem.Util;
 
 /**
  * This class loads data into the data repository and also
@@ -87,11 +88,11 @@ public class TestData {
 	@SuppressWarnings("serial")
 	public List<Author> allAuthors = new ArrayList<Author>() {
 		{
-			add(new Author("Joe", "Thomas", "641-445-2123", addresses.get(0), "A happy man is he."));
-			add(new Author("Sandra", "Thomas", "641-445-2123", addresses.get(0), "A happy wife is she."));
-			add(new Author("Nirmal", "Pugh", "641-919-3223", addresses.get(1), "Thinker of thoughts."));
-			add(new Author("Andrew", "Cleveland", "976-445-2232", addresses.get(2), "Author of childrens' books."));
-			add(new Author("Sarah", "Connor", "123-422-2663", addresses.get(3), "Known for her clever style."));
+			add(new Author(Util.getAlphaNumericString(6), "Joe", "Thomas", "641-445-2123", addresses.get(0), "A happy man is he."));
+			add(new Author(Util.getAlphaNumericString(6), "Sandra", "Thomas", "641-445-2123", addresses.get(0), "A happy wife is she."));
+			add(new Author(Util.getAlphaNumericString(6), "Nirmal", "Pugh", "641-919-3223", addresses.get(1), "Thinker of thoughts."));
+			add(new Author(Util.getAlphaNumericString(6), "Andrew", "Cleveland", "976-445-2232", addresses.get(2), "Author of childrens' books."));
+			add(new Author(Util.getAlphaNumericString(6), "Sarah", "Connor", "123-422-2663", addresses.get(3), "Known for her clever style."));
 		}
 	};
 	
--- src/librarysystem/AddBookForm.java
+++ src/librarysystem/AddBookForm.java
@@ -1,8 +1,6 @@
 package librarysystem;
 
 import javax.swing.*;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.DefaultTableModel;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,9 +23,13 @@ public class AddBookForm extends JPanel {
 	private List<Author> authors = new ArrayList<Author>();
 	private HashMap<String, Author> allAuthors;
 	private JComboBox<String> authorList;
+	private JComboBox<String> daysList;
 	private DefaultComboBoxModel<String> authorListModel;
-	private DefaultTableModel tableModel;
-	private JTable authorTable;
+	private DefaultComboBoxModel<String> dueDaysListModel;
+	private DefaultListModel<String> listModel = new DefaultListModel<String>();
+	private JList<String> mainList;
+	private JScrollPane mainScroll;
+	private JButton removeSelectedButton;
 
 	public void loadAuthors() {
 		this.authorListModel = new DefaultComboBoxModel<String>();
@@ -87,65 +89,72 @@ public class AddBookForm extends JPanel {
 		gbc.gridy = 3;
 		formPanel.add(checkoutLengthLabel, gbc);
 
-		JTextField checkoutLengthField = new JTextField(15);
+		// dropdown select length days
+		dueDaysListModel = new DefaultComboBoxModel<>();
+		dueDaysListModel.addElement("21");
+		dueDaysListModel.addElement("7");
+		daysList = new JComboBox<>(dueDaysListModel);
+
+		// Add the JList to a JScrollPane
 		gbc.gridx = 1;
 		gbc.gridy = 3;
-		formPanel.add(checkoutLengthField, gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		formPanel.add(daysList, gbc);
 
 		// Add the quantity label and text field
-		JLabel bookLabel = new JLabel("Book:");
+		JLabel bookLabel = new JLabel("Authors:");
 		gbc.gridx = 0;
 		gbc.gridy = 4;
 		gbc.fill = GridBagConstraints.NONE;
 		formPanel.add(bookLabel, gbc);
 
+		mainList = createJList();
+		mainList.setFixedCellWidth(70);
+		mainScroll = new JScrollPane(mainList);
+
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+		gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weighty = 1.0; // Allow table to expand vertically
+        formPanel.add(mainScroll, gbc);
+        
+		//remove item components
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        gbc.fill = GridBagConstraints.NONE;
+		removeSelectedButton = new JButton("Remove Selected");
+		formPanel.add(removeSelectedButton, gbc);
+
+		// dropdown select authors
 		authorListModel = new DefaultComboBoxModel<>();
 		loadAuthors();
 		authorList = new JComboBox<>(authorListModel);
 
 		// Add the JList to a JScrollPane
 		gbc.gridx = 1;
-		gbc.gridy = 4;
+		gbc.gridy = 6;
 		gbc.gridwidth = 1;
 		gbc.fill = GridBagConstraints.BOTH;
 		formPanel.add(authorList, gbc);
 
 		gbc.gridx = 2;
-		gbc.gridy = 4;
+		gbc.gridy = 6;
 		gbc.fill = GridBagConstraints.NONE;
 		JButton addAuthor = new JButton("Add Author");
 		formPanel.add(addAuthor, gbc);
 
 		// Add Book Button
 		JButton addButton = new JButton("Add Book");
-		gbc.gridx = 0;
-		gbc.gridy = 5;
-		gbc.gridwidth = 2; // Span across two columns to center the button
+		gbc.gridx = 1;
+		gbc.gridy = 7;
+		gbc.gridwidth = 1; // Span across two columns to center the button
 		gbc.anchor = GridBagConstraints.CENTER;
 		formPanel.add(addButton, gbc);
-
-//        String[] columnNames = {"Author ID", "First Name", "LastName", "Action"};
-//        tableModel = new DefaultTableModel(columnNames, 0) {
-//            @Override
-//            public boolean isCellEditable(int row, int column) {
-//                return false; // Make table read-only
-//            }
-//        };
-//        authorTable = new JTable(tableModel);
-//
-//        // Add button renderer and editor
-//        authorTable.getColumnModel().getColumn(3).setCellRenderer(new ButtonRenderer());
-//        authorTable.getColumnModel().getColumn(3).setCellEditor(new ButtonEditor(new JCheckBox(), authorTable));
-//        
-//        JScrollPane tableScrollPane = new JScrollPane(authorTable);
-//        gbc.gridx = 0;
-//        gbc.gridy = 3;
-//        gbc.gridwidth = 3;
-//        gbc.fill = GridBagConstraints.BOTH;
-//        gbc.weighty = 1.0; // Allow table to expand vertically
-//        add(tableScrollPane, gbc);
-
+        
 		add(formPanel, BorderLayout.NORTH);
+		
+		removeSelectedButton.addActionListener(new RemoveListener());
 
 		addAuthor.addActionListener(new ActionListener() {
 
@@ -156,8 +165,7 @@ public class AddBookForm extends JPanel {
 				String authorId = selectedAuthor.substring(0, selectedAuthor.indexOf(" - "));
 				Author author = allAuthors.get(authorId);
 				authors.add(author);
-
-//                addRowToTable(author);
+				listModel.addElement(selectedAuthor);
 			}
 
 		});
@@ -166,7 +174,7 @@ public class AddBookForm extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String isbn = isbnField.getText().trim();
-				String checkoutLength = checkoutLengthField.getText().trim();
+				String checkoutLength = (String) daysList.getSelectedItem();
 				String bookTitleString = bookTitleField.getText().trim();
 
 				if (authors.size() == 0 || isbn.equals("") || checkoutLength.equals("") || bookTitleString.equals("")) {
@@ -187,93 +195,28 @@ public class AddBookForm extends JPanel {
 				JOptionPane.showMessageDialog(AddBookForm.this, "Add book Successfull!!!", "Info",
 						JOptionPane.INFORMATION_MESSAGE);
 				isbnField.setText("");
-				checkoutLengthField.setText("");
+				daysList.setSelectedIndex(0);
 				bookTitleField.setText("");
+				listModel.removeAllElements();
 			}
 		});
 	}
-
-	private void addRowToTable(Author author) {
-		JButton removeButton = new JButton("Remove");
-		removeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				int rowIndex = authorTable.getSelectedRow();
-				if (rowIndex != -1) {
-					authors.remove(rowIndex); // Remove author from list
-					tableModel.removeRow(rowIndex); // Remove row from table
-				}
-			}
-		});
-
-		// Add row to table with author info and remove button
-		tableModel.addRow(new Object[] { authors.size(), // Author ID (for demonstration)
-				author.getFirstName(), author.getLastName(), removeButton });
+	
+	private JList<String> createJList() {
+		JList<String> ret = new JList<String>(listModel);
+		ret.setVisibleRowCount(4);
+		return ret;
 	}
-}
-
-class ButtonRenderer extends JButton implements TableCellRenderer {
-
-	public ButtonRenderer() {
-		setOpaque(true);
-	}
-
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		setText((value != null) ? value.toString() : "Remove");
-		return this;
-	}
-}
-
-// Button Editor class (for handling button clicks in the table)
-class ButtonEditor extends DefaultCellEditor {
-	protected JButton button;
-	private String label;
-	private boolean isPushed;
-	private JTable table;
-
-	public ButtonEditor(JCheckBox checkBox, JTable table) {
-		super(checkBox);
-		this.table = table;
-		button = new JButton();
-		button.setOpaque(true);
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				fireEditingStopped();
-			}
-		});
-	}
-
-	@Override
-	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-		label = (value != null) ? value.toString() : "Remove";
-		button.setText(label);
-		isPushed = true;
-		return button;
-	}
-
-	@Override
-	public Object getCellEditorValue() {
-		if (isPushed) {
-			// When the button is clicked, remove the current row
-			int selectedRow = table.getSelectedRow();
-			if (selectedRow != -1) {
-				((DefaultTableModel) table.getModel()).removeRow(selectedRow);
+	
+	class RemoveListener implements ActionListener {
+		public void actionPerformed(ActionEvent evt){
+			List<String> selected = mainList.getSelectedValuesList();
+			for(String s: selected) {
+				listModel.removeElement(s);
+				String authorId = s.substring(0, s.indexOf(" - "));
+				Author author = allAuthors.get(authorId);
+				authors.remove(author);
 			}
 		}
-		isPushed = false;
-		return label;
-	}
-
-	@Override
-	public boolean stopCellEditing() {
-		isPushed = false;
-		return super.stopCellEditing();
-	}
-
-	@Override
-	protected void fireEditingStopped() {
-		super.fireEditingStopped();
 	}
 }
